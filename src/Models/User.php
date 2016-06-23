c05c7a9b6e7fd40d4ae7a9a6db7558ea85749048
Jack Polgar <jack@polgar.id.au>
2016-06-23 14:18:32 +1000

    Set user table alias to `u`

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 class User extends \Avalon\Database\Model
 {
+    protected static $_tableAlias = 'u';
+
     protected static $_validations = [
         'username' => ['required', 'unique'],
         'password' => ['required']
